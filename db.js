82a0cd699ae5123a160ed6cb99a9ae68420223dc
Lopez_EJ <juanelopezg04@gmail.com>
2025-05-26 14:41:58 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql');
 
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // Coloca tu contraseña si tienes
+    database: 'artventory_db'
+});
+
+conexion.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a MySQL:', err);
+        return;
+    }
+    console.log('Conexión exitosa a MySQL');
+});
+
+module.exports = conexion;
